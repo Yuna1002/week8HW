6831e58db78afb832ee9b90f240a993722bbecf3
Yuna1002 <e2431666@gmail.com>
2022-09-04 23:59:35 +0800

    Update 2022-09-04T15:59:04.311Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,17 +1,19 @@
+"use strict";
+
 $(function () {
   // console.log('Hello Bootstrap5');
-
   var swiper = new Swiper(".artistSwiper", {
     spaceBetween: 24,
     slidesPerView: 1,
     breakpoints: {
       992: {
-        slidesPerView: 3,
-      },
+        slidesPerView: 3
+      }
     },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
-    },
+      clickable: true
+    }
   });
 });
+//# sourceMappingURL=all.js.map
